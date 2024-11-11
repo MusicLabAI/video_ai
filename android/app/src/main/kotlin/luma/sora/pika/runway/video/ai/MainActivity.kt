bce240071aa8f4fb37153d49291ed6c6ab2b00c1
xumingquan <xmq626336@gmail.com>
2024-11-11 14:13:01 +0800

    启动页调整

--- android/app/src/main/kotlin/luma/sora/pika/runway/video/ai/MainActivity.kt
+++ android/app/src/main/kotlin/luma/sora/pika/runway/video/ai/MainActivity.kt
@@ -1,5 +1,13 @@
 package luma.sora.pika.runway.video.ai
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+    }
+}
