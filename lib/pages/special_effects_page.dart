fa0bab6206da1dc9be28927d07ef6b04e742e256
xumingquan <xmq626336@gmail.com>
2024-11-26 17:38:04 +0800

    布局调整

--- lib/pages/special_effects_page.dart
+++ lib/pages/special_effects_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:video_ai/common/firebase_util.dart';
@@ -18,8 +17,8 @@ class SpecialEffectsPage extends StatefulWidget {
 
 class _SpecialEffectsPageState extends State<SpecialEffectsPage>
     with AutomaticKeepAliveClientMixin {
-    final CreateController _createCtr = Get.find<CreateController>();
-    final ScrollController _scrollController = ScrollController();
+  final CreateController _createCtr = Get.find<CreateController>();
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +28,8 @@ class _SpecialEffectsPageState extends State<SpecialEffectsPage>
       {
         "image": "https://placebear.com/400/300",
         "title": "Title 1: Beautiful Landscape",
-        "text": "Slide 1: Beautiful LandscapeSlide 1: Beautiful Landscape Slide 1: Beautiful Landscape Slide 1: Beautiful Landscape Slide 1: Beautiful Landscape"
+        "text":
+            "Slide 1: Beautiful LandscapeSlide 1: Beautiful Landscape Slide 1: Beautiful Landscape Slide 1: Beautiful Landscape Slide 1: Beautiful Landscape"
       },
       {
         "image": "https://picsum.photos/800/600",
@@ -47,66 +47,65 @@ class _SpecialEffectsPageState extends State<SpecialEffectsPage>
       backgroundColor: Colors.transparent,
       body: Stack(
         children: [
-          SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CarouselWidget(
-                  data: carouselData,
-                  height: 443.0,
-                  autoPlayInterval: const Duration(seconds: 5),
-                  showIndicator: true,
+          CarouselWidget(
+            data: carouselData,
+            height: 443.0,
+            autoPlayInterval: const Duration(seconds: 5),
+            showIndicator: true,
+          ),
+          SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                color: Colors.transparent,
+                padding: const EdgeInsets.only(left: 16),
+                height: 56,
+                width: double.infinity,
+                child: Row(
+                  children: [
+                    const Text(
+                      'Video AI',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: UiColors.cDBFFFFFF,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Spacer(),
+                    UserInfoWidget(),
+                    IconButton(
+                      onPressed: () {
+                        Get.to(() => const SettingsPage());
+                        FireBaseUtil.logEventButtonClick(
+                            PageName.specialEffectsPage, 'mine_button');
+                      },
+                      icon: Image.asset(
+                        'images/icon/ic_user.png',
+                        width: 24,
+                        height: 24,
+                      ),
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Text(
-                    'imageToVideo'.tr,
-                    style: const TextStyle(
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, top: 280, bottom: 20),
+                child: Text(
+                  'imageToVideo'.tr,
+                  style: const TextStyle(
                       fontSize: 14,
                       fontWeight: FontWeight.bold,
-                      color: Colors.white
-                    ),
-                  ),
+                      color: Colors.white),
                 ),
-                _bottomView()
-              ],
-            ),
-          ),
-          // 顶部导航和用户信息部分
-          SafeArea(
-            child: Container(
-              color: Colors.transparent,
-              padding: const EdgeInsets.only(left: 16),
-              height: 56,
-              width: double.infinity,
-              child: Row(
-                children: [
-                  const Text(
-                    'Video AI',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: UiColors.cDBFFFFFF,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const Spacer(),
-                  UserInfoWidget(),
-                  IconButton(
-                    onPressed: () {
-                      Get.to(() => const SettingsPage());
-                      FireBaseUtil.logEventButtonClick(
-                          PageName.specialEffectsPage, 'mine_button');
-                    },
-                    icon: Image.asset(
-                      'images/icon/ic_user.png',
-                      width: 24,
-                      height: 24,
-                    ),
-                  ),
-                ],
               ),
-            ),
-          ),
+              Expanded(child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: _bottomView(),
+              )),
+            ],
+          )),
+          // 顶部导航和用户信息部分
         ],
       ),
     );
@@ -117,30 +116,25 @@ class _SpecialEffectsPageState extends State<SpecialEffectsPage>
 
   Widget _bottomView() {
     return Obx(() {
-      return Container(
-          color: Colors.transparent,
-          padding: const EdgeInsets.only(left: 20, right: 20),
-          child: GridView.builder(
-            itemCount: _createCtr.effectsList.length,
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 154 / 212,
-              mainAxisSpacing: 16.0,
-              crossAxisSpacing: 12.0,
-            ),
-            itemBuilder: (BuildContext context, int index) {
-              return EffectsWidget(
-                model: _createCtr.effectsList[index],
-                onTap: (model) {
-                  _createCtr.selectEffects(model);
-                },
-              );
+      return GridView.builder(
+        itemCount: _createCtr.effectsList.length,
+        // shrinkWrap: false,
+        // physics: const NeverScrollableScrollPhysics(),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 154 / 212,
+          mainAxisSpacing: 16.0,
+          crossAxisSpacing: 12.0,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return EffectsWidget(
+            model: _createCtr.effectsList[index],
+            onTap: (model) {
+              _createCtr.selectEffects(model);
             },
-          ),
-        );
-      }
-    );
+          );
+        },
+      );
+    });
   }
 }
