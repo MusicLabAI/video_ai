7ad2f4a15e165f9e5dedd262e6f95ab3493b1164
xumingquan <xmq626336@gmail.com>
2024-12-04 14:16:13 +0800

    还原测试代码

--- lib/pages/create_page.dart
+++ lib/pages/create_page.dart
@@ -416,7 +416,7 @@ class _CreatePageState extends State<CreatePage>
                                 )
                               : null,
                           text: _createCtr.curEffects.value != null
-                              ? "dfkjsajfjasdjkfjdasjkfaskdfjkasjkdfjkasjkfjkasjkdfjklasd"
+                              ? _createCtr.curEffects.value!.tag ?? ""
                               : 'effect'.tr,
                           textSize: 10,
                           textColor: _createCtr.curEffects.value != null
